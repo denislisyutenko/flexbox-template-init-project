f859780f9bf6480e7aaa2fea5e3d2227c0f603ef
Denis Lisyutenko <denislisyutenko@gmail.com>
2022-07-25 16:22:24 +0300

    made a menu button

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,13 @@
-(function ($) {  
+/** @format */ 
 
-  $(document).ready(function() {  
-      $('body').niceScroll();
+(function ($) {
+  $(document).ready(function () {
+    $("body").niceScroll();
   });
 
-  $('.header-menu__btn').click(function() {
-    $(this).toggleClass('active');
-    $('.header-menu__list').toggleClass('active');
+  $(".header-menu__btn").click(function () {
+    $(this).toggleClass("header-menu__btn--active");
+    $(".header-menu__list").toggleClass("header-menu__list--active");
   });
 
 })(jQuery);
