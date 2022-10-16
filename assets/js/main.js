389443c361c984c0c39c67f518e4642be0147006
Denis Lisyutenko <denislisyutenko@gmail.com>
2022-10-16 13:44:09 +0300

    started making a left block

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,12 +2,12 @@
 
 (function ($) {
   $(document).ready(function () {
-    $("body").niceScroll();
+    $('body').niceScroll();
   });
 
-  $(".header-menu__btn").click(function () {
-    $(this).toggleClass("header-menu__btn--active");
-    $(".header-menu__list").toggleClass("header-menu__list--active");
+  $('.header-menu__btn').click(function () {
+    $(this).toggleClass('header-menu__btn--active');
+    $('.header-menu__list').toggleClass('header-menu__list--active');
   });
 
 })(jQuery);
